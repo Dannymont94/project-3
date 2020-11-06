bd71ba46370a42e0ac236d414bd9ed2a51e94d97
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-11-06 00:01:24 -0500

    created showSchema and added to TypeDefs

--- server/models/Show.js
+++ server/models/Show.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const showSchema = new Schema({
+  tvMazeId: {
+    type: Number,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
