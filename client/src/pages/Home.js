1e0d7d80a99457f2a2193c7826037f6661e4eb8e
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-11-08 14:25:56 -0500

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,44 +1,24 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import axios from 'axios';
-import ShowCard from '../components/ShowCard'
-import { UPDATE_SHOWS } from '../utils/actions';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Search from '../components/Search';
+import ShowCard from '../components/ShowCard';
 
 function Home() {
-    const [formState, setFormState] = useState('');
-
     const state = useSelector(state => state);
-    const dispatch = useDispatch();
-
-    function handleChange(event) {
-        setFormState(event.target.value);
-    }
-
-    async function getShows(event) {
-        event.preventDefault();
-
-        const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${formState}`);
-        dispatch({
-            type: UPDATE_SHOWS,
-            payload: response.data
-        });
-    }
 
     return (
-        <div className="">
+        <div>
             <h1>Home Page</h1>
-            <form className="flex-row search"onSubmit={getShows}>
-                <input id="searchInput" type="text" value={formState} onChange={handleChange}/>
-                <button type="submit">Search</button>
-            </form>
+            <Search mode={'Home'}/>
             <section className="grid-container">
-            {state.shows.length > 0 ? (
-                state.shows.map((showData, i) => (
-                    <ShowCard key={i} show={showData.show}/>
-                ))
-            ) : (
-                ''
-            )}
+
+                {state.shows.length > 0 ? (
+                    state.shows.map((showData, i) => (
+                        <ShowCard key={i} show={showData.show}/>
+                    ))
+                ) : (
+                    ''
+                )}
             </section>
         </div>
     );
