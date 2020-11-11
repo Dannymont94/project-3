a7fabacd7e23687ed308454cbc676c8564c14787
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-11-10 19:17:58 -0500

    added new mutation and fleshed out user query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -4,6 +4,39 @@ export const QUERY_USER = gql`
   {
     user {
       username
+      interestedCount
+      interested {
+        tvMazeId
+        name
+        summary
+        image
+        genres
+        network
+        status
+        rating
+      }
+      watchingCount
+      watching {
+        tvMazeId
+        name
+        summary
+        image
+        genres
+        network
+        status
+        rating
+      }
+      completedCount
+      completed {
+        tvMazeId
+        name
+        summary
+        image
+        genres
+        network
+        status
+        rating
+      }
     }
   }
 `;
